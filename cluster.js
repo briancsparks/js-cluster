482e4d2f0493f94e98c179622334a30c592056e8
Brian C Sparks <brian.c.sparks@hp.com>
2017-03-27 12:47:55 -0700

    First decent

--- cluster.js
+++ cluster.js
@@ -4,9 +4,47 @@
  */
 var sg              = require('sgsg');
 var _               = sg._;
+var redisLib        = require('redis');
 
 var lib = {};
 
+lib._mkKey = function(namespace, name) {
+  return ['service', namespace, name].join(':');
+};
+
+lib._registerService = function(redis, namespace, name, socket, ttl, callback) {
+  var key = lib._mkKey(namespace, name);
+
+  redis.set(key, socket, function(err, res) {
+    redis.expire(key, ttl);
+    return callback.apply(this, arguments);
+  });
+};
+
+lib._getServiceLocation = function(redis, namespace, name, callback) {
+  var key = lib._mkKey(namespace, name);
+  redis.get(key, function(err, res) {
+    if (err) { return callback(err); }
+    return callback(err, [res]);
+  });
+};
+
+lib.Service = function(namespace, host_, port_) {
+  var self = this;
+
+  var host  = host_ || 'localhost';
+  var port  = port_ || 6379;
+
+  var redis = redisLib.createClient(port, host);
+
+  self.registerService = function(name, socket, ttl, callback) {
+    return lib._registerService(redis, namespace, name, socket, ttl, callback);
+  };
+
+  self.getServiceLocation = function(name, callback) {
+    return lib._getServiceLocation(redis, namespace, name, callback);
+  };
+};
 
 _.each(lib, function(value, key) {
   exports[key] = value;
--- cluster.test.js
+++ cluster.test.js
@@ -1,8 +1,49 @@
 
-var cluster             = require('./cluster');
+var clusterLib          = require('./cluster');
 var test                = require('ava');
+
+var Service             = clusterLib.Service;
 
 test(t => {
   t.deepEqual([1, 2], [1, 2]);
+});
+
+//test.cb(t => {
+//  t.plan(1);
+//
+//  clusterLib._registerService("ns", "foo", "localhost:19999", 1, function(err, res) {
+//    clusterLib._getServiceLocation("ns", "foo", function(err, res) {
+//      //console.log(err, res);
+//      t.deepEqual(res, ['localhost:19999']);
+//      t.end();
+//    });
+//  });
+//});
+
+test.cb(t => {
+  t.plan(1);
+
+  var service = new Service('bar');
+  service.registerService('baz', 'localhost:18881', 1, function(err, res) {
+    service.getServiceLocation('baz', function(err, res) {
+      //console.log(err, res);
+      t.deepEqual(res, ['localhost:18881']);
+      t.end();
+    });
+  });
+});
+
+test.cb('cluster should expire service registrations', t => {
+  t.plan(1);
+
+  var service = new Service('barx', 'localhost', 6379);
+  service.registerService('bazx', 'localhost:17771', 1, function(err, res) {
+    setTimeout(function() {
+      service.getServiceLocation('baz', function(err, res) {
+        t.deepEqual(res, [null]);
+        t.end();
+      });
+    }, 2000);
+  });
 });
 

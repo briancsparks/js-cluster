1d2c06f569fa45b6817ef3ab895ac2219d591f53
Yoshi T. Munchakoopas <yoshi.t.munchakoopas@hp.com>
2017-03-28 06:35:53 +0000

    Refactoring

--- cluster.js
+++ cluster.js
@@ -8,24 +8,34 @@ var redisLib        = require('redis');
 
 var lib = {};
 
-lib._mkKey = function(namespace, name) {
+var _mkKey = lib._mkKey = function(namespace, name) {
   return ['service', namespace, name].join(':');
 };
 
-lib._registerService = function(redis, namespace, name, socket, ttl, callback) {
-  var key = lib._mkKey(namespace, name);
+lib._mkUniqKey = function(namespace, name, uniq) {
+  return [_mkKey(namespace, name), uniq].join(':');
+};
 
-  redis.set(key, socket, function(err, res) {
-    redis.expire(key, ttl);
-    return callback.apply(this, arguments);
+lib._registerService = function(redis, namespace, name, socket, uniq, ttl, callback) {
+  var key     = lib._mkKey(namespace, name);
+  var uniqKey = lib._mkUniqKey(namespace, name, uniq);
+
+  return redis.psetex(uniqKey, ttl, socket, function(err, res) {
+    return redis.sadd(key, uniqKey, function(err, res) {
+      return callback.apply(this, arguments);
+    });
   });
 };
 
 lib._getServiceLocation = function(redis, namespace, name, callback) {
   var key = lib._mkKey(namespace, name);
-  redis.get(key, function(err, res) {
+
+  // Get all the members of the set -- all the instances of the service
+  return redis.smembers(key, function(err, keys) {
     if (err) { return callback(err); }
-    return callback(err, [res]);
+    return redis.mget(keys, function(err, res) {
+      return callback(err, _.compact([res]));
+    });
   });
 };
 
@@ -37,13 +47,36 @@ lib.Service = function(namespace, host_, port_) {
 
   var redis = redisLib.createClient(port, host);
 
-  self.registerService = function(name, socket, ttl, callback) {
-    return lib._registerService(redis, namespace, name, socket, ttl, callback);
+  self.registerService = function(name, socket, uniq, ttl, callback) {
+    return lib._registerService(redis, namespace, name, socket, uniq, ttl, callback);
   };
 
   self.getServiceLocation = function(name, callback) {
     return lib._getServiceLocation(redis, namespace, name, callback);
   };
+};
+
+var getServices = lib.getServices = function(serviceList, name, callback) {
+  var services, error;
+
+  return sg.__each(serviceList, function(clusterService, nextService) {
+
+    // Once we have services, do not need to look for more
+    if (services) { return nextService(); }
+
+    return clusterService.getServiceLocation(name, function(err, results) {
+      error = error || err;
+      if (!err && results.length > 0) {
+        services = _.map(results, function(str) { return str.replace(/[/]$/g, ''); });
+      }
+
+      return nextService();
+    });
+
+  }, function() {
+    if (error) { return callback(error); }
+    return callback(null, services);
+  });
 };
 
 _.each(lib, function(value, key) {
